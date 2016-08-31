65645fd14cbe660ac65699e405623f2aae76ee3a
Jun Ueoka <nimzo.i.d.6689@gmail.com>
2016-09-01 07:37:59 +0900

    Added Get-HMacSHA1 Cmdlet

---
+++ scripts/GetHMacSHA1.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    HMacSHA1の取得スクリプト
+
+    @author nimzo
+#>
+$ErrorActionPreference = "stop"
+
+function Get-HMACSHA1([string]$publicKey, [string]$privateKey){
+    $hmacsha = New-Object System.Security.Cryptography.HMACSHA1
+    $hmacsha.Key = [System.Text.Encoding]::ASCII.GetBytes($privateKey)
+
+    [byte[]]$publicKeyBytes = [System.Text.Encoding]::ASCII.GetBytes($publicKey)
+    [byte[]]$hash = $hmacsha.ComputeHash($publicKeyBytes)
+    return [System.BitConverter]::ToString($hash).Replace("-","").ToLower()
+}
+
+function ConvertTo-Base64([string] $toEncode){
+    [byte[]]$toEncodeAsBytes = [System.Text.ASCIIEncoding]::ASCII.GetBytes($toEncode)
+    [string]$returnValue = [System.Convert]::ToBase64String($toEncodeAsBytes)
+    return $returnValue
+}
+
+Get-HMACSHA1 -publicKey abcdefg -privateKey 12345
