ed7b2094023d8190f963c6f8db6cbbdf413fb655
Jun Ueoka <nimzo.i.d.6689@gmail.com>
2016-09-14 21:03:47 +0900

    Removed Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-$scriptPath = $MyInvocation.MyCommand.Path
-$invokedScripts = Join-Path (Split-Path $scriptPath -Parent) "scripts"
-
-gci $invokedScripts -Include *.ps1 -Recurse -Force | % {&($_.FullName)}
-
-#Override
-function Prompt() {
-    (Split-Path $pwd -Qualifier) + "~" + (Split-Path $pwd -Leaf) + "> "
-}
