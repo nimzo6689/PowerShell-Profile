b4ffa6d87a3074e8b440480362a571e4f82c9763
Jun Ueoka <nimzo.i.d.6689@gmail.com>
2016-09-14 21:06:37 +0900

    Added some modules.

---
+++ Modules/OracleDB/_createManifest.ps1
@@ -0,0 +1,36 @@
+﻿$script:module = "OracleDB"
+$script:moduleVersion = "0.0.2"
+$script:description = "PowerShell Oracle Database library";
+$script:copyright = "14/Sept/2016 -"
+$script:RequiredModules = @()
+$script:clrVersion = "4.0.0.0" 
+
+$script:functionToExport = @(
+        "Set-ConnectionString",
+        "Get-OracleDB",
+        "Update-OracleDB",
+        "Formart-SQL"
+)
+
+$script:variableToExport = ""
+$script:AliasesToExport = @()
+
+$script:moduleManufest = @{
+    Path = "$module.psd1";
+    Author = $env:USERNAME;
+    CompanyName = "";
+    Copyright = ""; 
+    ModuleVersion = $moduleVersion;
+    Description = $description;
+    PowerShellVersion = "5.0";
+    DotNetFrameworkVersion = "4.0";
+    ClrVersion = $clrVersion;
+    RequiredModules = $RequiredModules;
+    NestedModules = "$module.psm1";
+    CmdletsToExport = "*";
+    FunctionsToExport = $functionToExport;
+    VariablesToExport = $variableToExport;
+    AliasesToExport = $AliasesToExport;
+}
+
+New-ModuleManifest @moduleManufest
